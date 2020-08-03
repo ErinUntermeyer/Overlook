4caf5ce7e79393584839fe577f952348a12aeaac
Erin Untermeyer <aerinuntermeyer@gmail.com>
2020-08-03 10:52:32 -0600

    Write test for display customer details #51

--- test/domUpdates-test.js
+++ test/domUpdates-test.js
@@ -6,13 +6,15 @@ chai.use(spies);
 import domUpdates from '../src/domUpdates';
 
 describe('domUpdates', function() { 
-	let section, sectionClass, booking, variableName;
+	let section, sectionClass, bookings, variableName, customerName, rooms;
 
 	beforeEach(function() {
 		section = 'this section';
 		sectionClass = 'this section class';
-		booking = 'this booking';
+		bookings = 'these bookings';
 		variableName = 'this variable name'
+		customerName = 'erin'
+		rooms = 'these rooms'
 		global.document = {};
 		chai.spy.on(document, ['querySelector'], function () { return {} })
 	})
@@ -30,11 +32,11 @@ describe('domUpdates', function() {
 
 	it('should create room cards', function () {
 		chai.spy.on(domUpdates, ['createRoomCard', 'createBookRoomCard'], function () { return {} })
-		domUpdates.createRoomCard(section, sectionClass, booking, variableName);
+		domUpdates.createRoomCard(section, sectionClass, bookings, variableName);
 		domUpdates.createBookRoomCard(section, sectionClass, variableName);
 
 		expect(domUpdates.createRoomCard).to.have.been.called(1);
-		expect(domUpdates.createRoomCard).to.have.been.called.with(section, sectionClass, booking, variableName);
+		expect(domUpdates.createRoomCard).to.have.been.called.with(section, sectionClass, bookings, variableName);
 		expect(domUpdates.createBookRoomCard).to.have.been.called(1);
 		expect(domUpdates.createBookRoomCard).to.have.been.called.with(section, sectionClass, variableName);
 	})
@@ -44,5 +46,13 @@ describe('domUpdates', function() {
 		domUpdates.displayCustomerLandingPage();
 
 		expect(domUpdates.displayCustomerLandingPage).to.have.been.called(1);
+	})
+
+	it('should display the customer details', function () {
+		chai.spy.on(domUpdates, ['displayCustomerDetails'], function () { return {} })
+		domUpdates.displayCustomerDetails(customerName, bookings, rooms);
+
+		expect(domUpdates.displayCustomerDetails).to.have.been.called(1);
+		expect(domUpdates.displayCustomerDetails).to.have.been.called.with(customerName, bookings, rooms);
 	})
 })
